7464d88ffd4b4160fc8f5e639feec5cca4eb60cb
MiguelLeme <miguellemepps13@gmail.com>
2024-09-17 22:03:04 -0300

    Tela de cadastro e tela de login

--- App.js
+++ App.js
@@ -1,127 +1,34 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert, SafeAreaView } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/login'; 
+import CadastroScreen from './screens/cadastro'; 
+import Home from './screens/Home';
 
-const CadastroScreen = () => {
-  const [nomeCompleto, setNomeCompleto] = useState('');
-  const [cpf, setCpf] = useState('');
-  const [email, setEmail] = useState('');
-  const [telefone, setTelefone] = useState('');
-  const [matricula, setMatricula] = useState('');
-  const [curso, setCurso] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confirmarSenha, setConfirmarSenha] = useState('');
+const Stack = createStackNavigator();
 
-  const handleCadastro = () => {
-    if (senha !== confirmarSenha) {
-      Alert.alert("Erro", "As senhas não coincidem!");
-      return;
-    }
-    // Lógica para envio dos dados
-    Alert.alert("Sucesso", "Cadastro realizado!");
-  };
-
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>BlaBlacar</Text>
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Nome completo"
-        placeholderTextColor="#ccc"
-        value={nomeCompleto}
-        onChangeText={setNomeCompleto}
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="CPF"
-        placeholderTextColor="#ccc"
-        value={cpf}
-        onChangeText={setCpf}
-        keyboardType="numeric"
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        placeholderTextColor="#ccc"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Telefone"     
-        placeholderTextColor="#ccc"
-        value={telefone}
-        onChangeText={setTelefone}
-        keyboardType="phone-pad"
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Matrícula (RA)"
-        placeholderTextColor="#ccc"
-        value={matricula}
-        onChangeText={setMatricula}
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Curso"
-        placeholderTextColor="#ccc"
-        value={curso}
-        onChangeText={setCurso}
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        placeholderTextColor="#ccc"
-        value={senha}
-        onChangeText={setSenha}
-        secureTextEntry
-      />
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Confirmar Senha"
-        placeholderTextColor="#ccc"
-        value={confirmarSenha}
-        onChangeText={setConfirmarSenha}
-        secureTextEntry
-      />
-      
-      <Button title="Cadastrar" onPress={handleCadastro} />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Cadastro"
+          component={CadastroScreen}      
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+      <Stack.Screen 
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#12023d', // Azul escuro próximo do roxo
-  },
-  title: {
-    marginTop: 40,
-    fontSize: 48,
-    fontWeight: 'bold',
-    marginBottom: 16,
-    textAlign: 'center',
-    color: '#fff', // Cor branca para o texto
-  },
-  input: {
-    color: '#fff',
-    margin: 10,
-    height: 50,
-    borderColor: '#4f0466',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 10,
-    borderRadius: 24,
-  },
-});
-
-export default CadastroScreen;
+export default App;
---
+++ screens/login.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  const [matricula, setMatricula] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleLogin = () => {
+    if (matricula === '' || senha === '') {
+      Alert.alert("Erro", "Preencha todos os campos!");
+      return;
+    }
+    Alert.alert("Sucesso", "Login realizado!");
+
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={80}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <SafeAreaView style={styles.innerContainer}>
+          <Text style={styles.title}>VaiJunto - Login</Text>
+          
+          <TextInput
+            style={styles.input}
+            placeholder="Matrícula (RA)"
+            placeholderTextColor="#ccc"
+            value={matricula}
+            onChangeText={setMatricula}
+          />
+          
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            placeholderTextColor="#ccc"
+            value={senha}
+            onChangeText={setSenha}
+            secureTextEntry
+          />
+          
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.registerButton} onPress={() => navigation.navigate('Cadastro')}>
+            <Text style={styles.registerButtonText}>Fazer cadastro</Text>
+          </TouchableOpacity>
+          
+        </SafeAreaView>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#12023d', 
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    marginTop: 40,
+    fontSize: 48,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  input: {
+    color: '#fff',
+    margin: 10,
+    height: 50,
+    borderColor: '#4f0466',
+    borderWidth: 4,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    borderRadius: 24,
+  },
+  button: {
+    backgroundColor: 'transparent',
+    borderColor: 'orange',
+    borderWidth: 2,
+    borderRadius: 24,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'yellow',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  registerButton: {
+    backgroundColor: 'transparent',
+    borderColor: 'yellow',
+    borderWidth: 2,
+    borderRadius: 24,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  registerButtonText: {
+    color: 'orange',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
